3ae66bd978c3815766e68a3f4bee86443409b1e9
Stalin <nix_olympica@tut.by>
2018-08-31 15:08:29 +0300

    full version, interactions only of two object

--- src/Begin.java
+++ src/Begin.java
@@ -14,16 +14,30 @@ public class Begin {
 		 Unit human = new Unit(50,10,"human");
 		 Unit terminator = new Unit(100,20,"terminator");
 		 int count = 1;
+		 boolean contact = false;
 		 
 // ..MAIN STREAM	 
 		 while(!s.equals("exit")){
 			 System.out.println();
 			 System.out.println();
 			 System.out.println("begin-------------" + count + "------------------");
+			 //SYSTEM STATUS
+			 System.out.println("..system status:");
+			 system_status(human);
+			 system_status(terminator);
+			 System.out.println("contact status: " + contact);
+			 System.out.println("****");
+			 //MAIN MENU
 			 System.out.println("..main menu:");
+			 System.out.println("command - create human");
+			 System.out.println("command - delete human");
+			 System.out.println("command - create terminator");
+			 System.out.println("command - delete terminator");
+			 
+			 System.out.println("command - launch contact");
 			 System.out.println("command - begin fight");
 			 System.out.println("command - exit from program");
-			 System.out.println("Your command is: " + s);
+			 System.out.println("Entere Your command is:");
 			 System.out.println();
 			 s = choice.readLine();
 			 System.out.println("....result");
@@ -32,13 +46,13 @@ public class Begin {
 			 
 			 
 //***	ВЗАИМОДЕЙСТВИЕ!!!
-//		НАБОР УСЛОВИЙ
-			 	if(s.equals("begin fight")){
+//		НАБОР УСЛОВИЙ FOR FIGHT!!!
+			 	if(s.equals("begin fight") && contact == true){
 			 		System.out.println("..fight begin");
 			 			
 			 			while(!s.equals("finish fight")){
-			 				System.out.println("fight, first bum");
-			 				
+			 				System.out.println("fight-------------" + count + "------------------");
+			 				count++;
 			 				
 //АЛГОРИТМ ВЗАИМОДЕЙСТВИЯ///////////////////////////////////////////////////////////////////////			 				
 			 				human.dead(terminator.weapon);
@@ -55,12 +69,38 @@ public class Begin {
 			 				if(terminator.life <= 0){
 			 					System.out.println("terminator is dead");break;
 			 				}
+			 				System.out.println("---------------------------------end");
 			 				s = choice.readLine();
 			 				System.out.println(s);
 			 				if(s.equals("finish fight"))break;
 			 				
 			 			}
-			 	}	
+			 	}
+//***	ВЗАИМОДЕЙСТВИЕ!!!
+//				НАБОР УСЛОВИЙ FOR CREATE AND DELETE!!!
+			 	if(s.equals("create human")){
+			 		human.life = 50;
+			 	}
+			 	
+			 	if(s.equals("create terminator")){
+			 		terminator.life = 100;
+			 	}
+///////////////////////////////DELETE///////////////////////////////////////////
+				if(s.equals("delete human")){
+			 		human.life = 0;
+			 	}
+			 	
+			 	if(s.equals("delete terminator")){
+			 		terminator.life = 0;
+			 	}
+/////////////////////////////CONTACT/////////////////////////////////////////////
+			 	if(s.equals("launch contact")){
+			 		contact = true;
+			 	}
+			 	
+			 	if(s.equals("stop contact")){
+			 		contact = false;
+			 	}
 //..END MAIN STREAM
 			 }
 		 
@@ -68,6 +108,10 @@ public class Begin {
 		 System.out.println();
 		 System.out.println("....total end");
 
+	}
+	public static void system_status(Unit human){
+		//System.out.println("..system status:");
+		System.out.println(human.kind + " life: " + human.life);
 	}
 
 }
